19fc64d49883bf8043f35498fbf2a800378ca1f8
REDDY janakiram <112166958+jana-ki-ram@users.noreply.github.com>
2023-05-01 18:51:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+function game(){
 var randomnum=Math.ceil(Math.random()*6);
 document.querySelector(".player1dice").setAttribute("src","images/dice"+randomnum+".png")
 var randomnum1=Math.ceil(Math.random()*6);
@@ -13,4 +13,6 @@ else if(randomnum < randomnum1){
 }
 else{
 document.querySelector("h1").innerHTML="Draw"
-}
+}}
+document.querySelector("button").setAttribute("onclick","game()")
+
